41af3aac00547f59d9f2a815f254fe64385328c3
Suyog-igen <suyog.karki@igentax.com>
2026-06-19 10:50:22 -0500

    Fix variable name in API request for fruit info

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ if ingredients_list:
     
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-        st.subheader(fruits_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        st.subheader(fruits_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)  
         sf_df = smoothiefroot_response.json()
         st.dataframe(data=sf_df, use_container_width=True)
 
