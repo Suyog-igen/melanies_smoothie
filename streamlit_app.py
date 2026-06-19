baa962420824c9e397df597f53bf2bf571b39032
Suyog-igen <suyog.karki@igentax.com>
2026-06-19 09:49:17 -0500

    Replace get_active_session with st.connection
    update session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title("Customize your smoothie 🥤")
 st.write("Choose your own fruits")
 
-session = get_active_session()
+cnx = st.connection('snowflake')
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
 ingredients_list = st.multiselect(
